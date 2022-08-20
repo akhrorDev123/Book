4628f2355704b60a52bb8fc337c494d92af822b1
shaxzod <ahrorisaev73@gmail.com>
2022-08-20 17:40:35 +0500

    Test push

--- src/main/java/com/onevizion/book/controller/BookController.java
+++ src/main/java/com/onevizion/book/controller/BookController.java
@@ -4,7 +4,6 @@ import com.onevizion.book.entity.Book;
 import com.onevizion.book.service.BookService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 
